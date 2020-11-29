b8f0281188164e5d2837ec874548c8581f613872
Fankasy <fankasy@hotmail.com>
2020-11-29 15:03:48 -0800

    init

---
+++ src/primeNumber/Solution.java
@@ -0,0 +1,21 @@
+package primeNumber;
+import java.util.*;
+public class Solution {
+	
+	public static void main (String [] args) {
+		System.out.println("hello");
+	}
+	
+	public static long getSumOfPrimeBelow(int n) {
+		List<Long> result = new ArrayList<>();
+		
+		
+		return 0;
+	}
+	public static boolean isPrimeNumber(int n){
+		
+		
+		return false;
+	}
+
+}
